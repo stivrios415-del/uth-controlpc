02d1a1fce5c21b9ac098b796897be13c5b20920d
Ronald Matute <stivrios415@gmail.com>
2026-07-02 11:44:50 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <--- NUEVA LÍNEA
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import logo from './logo.png'; // <--- Importa el logo desde src/
+
+// Asignar el logo como favicon
+const favicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+favicon.type = 'image/png';
+favicon.rel = 'shortcut icon';
+favicon.href = logo;
+document.head.appendChild(favicon);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
