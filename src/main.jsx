02fc7662911cc358582e3a0893687b2c17ec5954
Ronald Matute <stivrios415@gmail.com>
2026-07-02 11:52:24 -0600

    Add favicon support and import bootstrap JS
    Added favicon support by importing logo.png and updating the document head.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // <--- NUEVA LÍNEA
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// ===== AGREGAR FAVICON DESDE src/logo.png =====
+import logo from './logo.png';
+
+const favicon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+favicon.type = 'image/png';
+favicon.rel = 'shortcut icon';
+favicon.href = logo;
+document.head.appendChild(favicon);
+// ===== FIN FAVICON =====
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
